29830c01296f5546c9da0fffa5ce0f627dc47bde
Murielle PIRON <murielle.piron@cndbs-binche.be>
2025-01-14 22:06:43 +0100

    Etape2: Augmentation du contenu de l'entête + première fonction

--- index.php
+++ index.php
@@ -1,33 +1,16 @@
-<!-- etape1.php -->
-<!-- inclusion des variables -->
+<!-- etape2.php -->
+<!-- première fonction afficher carte -->
 <?php
     include_once('variables.php');
-?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jeu UNO - Etape 1</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" type="image/jpg" href="cartes/uno.png" />
-</head>
-
-<body>
-
-    <!-- inclusion de l'entête du site -->
-    <?php include_once('header.php'); ?>
+    include_once('functions.php');
+    include_once('header.php'); ?>
 
     <div id="container">
         <h1>Cartes dans un deck neuf</h1>
         <?php
             shuffle($cartes) ;
-        foreach($cartes as $carte) : ?>
-            <img class="carte" src="cartes/<?php echo $carte['image']; ?>" alt="<?php echo $carte['nom']; ?>">
-        <?php endforeach ?>
+            afficherCartes($cartes);
+            ?>
     </div>
 
     <!-- inclusion du bas de page du site -->
